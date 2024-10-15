9bbafc5249a38a892ff18f040761503b83864c16
SrinuGatta <mm.srinug@gmail.com>
2024-10-15 15:38:33 +0530

    Order dashboard page added

---
+++ Novel.Models/ViewModels/DashboardView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novel.Models.ViewModels
+{
+    public class DashboardView
+    {
+        public int TotalOrders { get; set; }
+        public int TotalInProcess { get; set; }
+        public int TotalApproved { get; set; }
+        public int TotalPending { get; set; }
+        public int TotalCompleted { get; set; }
+    }
+}
